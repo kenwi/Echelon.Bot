8d0e9c2b9548e65a7be08aef5784949d4713ca5c
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2025-06-09 04:13:06 +0200

    fix: use Author GlobalName in logging

--- Services/LoggingService.cs
+++ Services/LoggingService.cs
@@ -54,7 +54,7 @@ public class LoggingService
             .ToString(_dateTimeFormat, _culture);
             
         // Create the log entry
-        var logEntry = $"[{timestamp}] {message.Author.Username}: {message.Content}";
+        var logEntry = $"[{timestamp}] <{message.Author.GlobalName}> {message.Content}";
         if (message.Attachments.Count > 0)
         {
             logEntry += $"\nVedlegg: {string.Join(", ", message.Attachments.Select(a => a.Url))}";
