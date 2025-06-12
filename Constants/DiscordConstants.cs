09a4b340ebf104f0ae0d027ea3c247c8643ce6b2
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2025-06-12 06:06:22 +0200

    refactor: update constants

--- Constants/DiscordConstants.cs
+++ Constants/DiscordConstants.cs
@@ -6,11 +6,22 @@ public static class DiscordConstants
     {
         public const string SpotifyTrackPrefix = "https://open.spotify.com/track/";
         public const string SpotifyPlaylistPrefix = "https://open.spotify.com/playlist/";
+        public const string SpotifyAlbumPrefix = "https://open.spotify.com/album/";
+        public const string SpotifyArtistPrefix = "https://open.spotify.com/artist/";
     }
     
     public static class ConfigSections
     {
         public const string Discord = "Discord";
         public const string LoggingSettings = "Logging:Settings";
+        public const string SpotifyMessageParserService = "Discord:SpotifyMessageParserService";
+        public const string DefaultMessageParserService = "Discord:DefaultMessageParserService";
+    }
+    
+    public static class MessageTypes
+    {
+        public const string SpotifyTrack = "SpotifyTrack";
+        public const string SpotifyRequest = "SpotifyRequest";
+        public const string Default = "Default";
     }
 }
